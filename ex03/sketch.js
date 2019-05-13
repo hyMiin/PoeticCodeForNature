e84c1bf28ab3d3dd2b74c6ca141062f9fc765363
hyMiin <49216737+hyMiin@users.noreply.github.com>
2019-05-13 22:20:27 +0900

    Update ex03

--- ex03/sketch.js
+++ ex03/sketch.js
@@ -1,74 +1,143 @@
-let p = [];
+let fireworks = [];
+let gravity;
 
 function setup() {
-  title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목");
+  title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> BloomFireworks");
    title.position(20, 0);
   canvas = createCanvas(400, 400);
   canvas.position(20, 60);
   canvas.class("artwork");
-  colorMode(RGB, 100, 100, 100, 255);
+  colorMode(HSB);
+  gravity = createVector(0, 0.2);
+  stroke(255);
+  strokeWeight(4);
+  background(0);
 }
 
 function draw() {
-  background(100);
-  for (let i = 0; i < p.length; i++) {
-    p[i].addParticle();
-    for (let j = 0; j < p[i].particles.length; j++) {
-      p[i].particles[j].run(i);
-      if (p[i].particles[j].isDead()) {
-        p[i].particles.splice(j, 1);
-        j--;
-      }
+  colorMode(RGB);
+  background(0, 0, 0, 25);
+
+  if (random(1) < 0.03) {
+    fireworks.push(new Firework());
+  }
+
+  for (let i = fireworks.length - 1; i >= 0; i--) {
+    fireworks[i].update();
+    fireworks[i].show();
+
+    if (fireworks[i].done()) {
+      fireworks.splice(i, 1);
     }
   }
 }
 
-class Particle {
-  constructor(l) {
-    this.acc = createVector(0, 0.005);
-    this.vel = createVector(random(-1, 1), random(-1, 1));
-    this.pos = l;
-    this.lifespan = 255.0;
+
+class Firework {
+  constructor() {
+    this.hu = random(255);
+    this.firework = new Particle(random(width), height, this.hu, true);
+    this.exploded = false;
+    this.particles = [];
   }
-  run(i) {
-    this.update();
-    this.display(i);
-  }
-  update() {
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.lifespan -= 2.0;
-  }
-  display(i) {
-    noStroke();
-    fill(p[i].col, 0, 0, this.lifespan);
-    rect(this.pos.x, this.pos.y, 12, 12);
-  }
-  isDead() {
-    if (this.lifespan < 0.0) {
+
+  done() {
+    if (this.exploded && this.particles.length === 0) {
       return true;
     } else {
       return false;
     }
   }
-}
 
-function mouseClicked() {
-  p.push(new ParticleSystem(mouseX, mouseY));
-  p.push(new ParticleSystem(mouseX - 10, mouseY));
-  p.push(new ParticleSystem(mouseX + 10, mouseY));
-  p.push(new ParticleSystem(mouseX, mouseY - 10));
-  p.push(new ParticleSystem(mouseX, mouseY + 10));
-}
+  update() {
+    if (!this.exploded) {
+      this.firework.applyForce(gravity);
+      this.firework.update();
 
-class ParticleSystem {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.col = random(50, 255);
-    this.particles = [];
+      if (this.firework.vel.y >= 0) {
+        this.exploded = true;
+        this.explode();
+      }
+    }
+
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      this.particles[i].applyForce(gravity);
+      this.particles[i].update();
+
+      if (this.particles[i].done()) {
+        this.particles.splice(i, 1);
+      }
+    }
   }
-  addParticle() {
-    this.particles.push(new Particle(createVector(this.x, this.y)));
+
+  explode() {
+    for (let i = 0; i < 100; i++) {
+      let p = new Particle(this.firework.pos.x, this.firework.pos.y, this.hu, false);
+      this.particles.push(p);
+    }
+  }
+
+  show() {
+    if (!this.exploded) {
+      this.firework.show();
+    }
+
+    for (var i = 0; i < this.particles.length; i++) {
+      this.particles[i].show();
+    }
+  }
+}
+
+
+class Particle {
+  constructor(x, y, hu, firework) {
+    this.pos = createVector(x, y);
+    this.firework = firework;
+    this.lifespan = 255;
+    this.hu = hu;
+    this.acc = createVector(0, 0);
+    if (this.firework) {
+      this.vel = createVector(0, random(-12, -8));
+    } else {
+      this.vel = p5.Vector.random2D();
+      this.vel.mult(random(8, 10));
+    }
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  update() {
+    if (!this.firework) {
+      this.vel.mult(0.9);
+      this.lifespan -= 4;
+    }
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  done() {
+    if (this.lifespan < 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  show() {
+    colorMode(HSB);
+
+    if (!this.firework) {
+      strokeWeight(2);
+      stroke(this.hu, random(255), random(255), this.lifespan);
+      rect(this.pos.x, this.pos.y, 2, 2);
+    } else {
+      strokeWeight(4);
+      stroke(this.hu, 255, 255);
+    }
+
+    point(this.pos.x, this.pos.y);
   }
 }
